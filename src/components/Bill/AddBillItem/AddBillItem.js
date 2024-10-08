e9c69e283e8f8d8b1ed16073d8acc26cbb20ece2
Pedro Henestroza <phenestroza@gmail.com>
2024-10-08 10:56:03 -0300

    Improve member selection

--- src/components/Bill/AddBillItem/AddBillItem.js
+++ src/components/Bill/AddBillItem/AddBillItem.js
@@ -4,28 +4,41 @@ import styles from "./AddBillItem.module.scss";
 
 const AddBillItem = ({ members }) => {
 
-    const [allSelected, setAllSelected] = useState(true);
-    const [selectedItems, setSelectedItems] = useState([]);
+    const [allSelected, setAllSelected] = useState(false);
+    const [selectedMembers, setSelectedMembers] = useState([]);
 
     const selectAllHandler = () => {
         setAllSelected(!allSelected);
-        if(allSelected){
-            setSelectedItems(members.map(member => member.id));
+        if(!allSelected){
+            setSelectedMembers(members.map(member => member.id));
         }else{
-            setSelectedItems([]);
+            setSelectedMembers([]);
         }
     }
 
     const selectItemHandler = (id) => {
-        if(selectedItems.includes(id)){
-            setSelectedItems(selectedItems.filter(item => item !== id));
+        if(selectedMembers.includes(id)){
+          const newMembers = selectedMembers.filter(item => item !== id);
+            setSelectedMembers(newMembers);
+            if(newMembers.length < 1){
+              setAllSelected(false);
+            }
         }else{
-            setSelectedItems([...selectedItems, id]);
+          const newMembers = [...selectedMembers, id]
+            setSelectedMembers(newMembers);
+            if(newMembers.length == members.length){
+              setAllSelected(true);
+            }
         }
     }
 
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log(e);
+    }
+
   return (
-    <form className={styles.addBillItem}>
+    <form className={styles.addBillItem} onSubmit={submitHandler}>
       <div className={styles.addBillItem__container}>
         <div className={styles.addBillItem__container__left}>
           <input type="text" placeholder="Item name" required />
@@ -34,12 +47,12 @@ const AddBillItem = ({ members }) => {
             {members.map((member, index) => (
               <div key={index} className={styles.addBillItem__list__item}>
                 <label htmlFor={`${member.name}-${member.id}`}>{member.name}</label>
-                <input id={`${member.name}-${member.id}`} checked={selectedItems.includes(member.id)} onClick={() => selectItemHandler(member.id)} type="checkbox" style={{ backgroundColor: member.color }} />  
+                <input id={`${member.name}-${member.id}`} checked={selectedMembers.includes(member.id)} onChange={() => selectItemHandler(member.id)} type="checkbox" style={{ backgroundColor: member.color }} />  
               </div>
             ))}
             <div className={`${styles.addBillItem__list__item} ${styles.addBillItem__list__all} `}>
               <label htmlFor="all">All</label>
-              <input id="all" type="checkbox" onClick={selectAllHandler} />
+              <input id="all" type="checkbox" checked={allSelected} onChange={selectAllHandler} />
             </div>
           </div>
         </div>
